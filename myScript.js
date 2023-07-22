07008ae8dc91fd1907e959014144ad753ac176cd
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-07-22 22:56:53 +0100

    numbers display, start of equal function

--- myScript.js
+++ myScript.js
@@ -47,9 +47,43 @@ function calc(){
 console.log(calc())
 
 const total = document.getElementById("total");
+const op = document.getElementById("op");
 
-change = function(){
-  total.innerHTML = "1000"
+// Capturing all the buttons and attaching event listener to each
+document.querySelectorAll('button').forEach(e =>{
+  e.addEventListener("click", ()=> {
+    console.log(e)
+    digit_pressed(e.textContent);
+    showNumber(e.textContent);
+  });
+});
+
+// This is just to show what button is being pressed 
+function digit_pressed(digit) {
+  console.log("digit pressed" + digit);
+}
+
+//This takes the digit and displays it to the calc screen
+function showNumber(digit) {
+  numTotal += digit
+  total.innerHTML = numTotal
+  console.log(numTotal)
 };
 
-change()
+
+let numTotal = "";
+
+// Adds event for the equals key
+op.addEventListener("click", ()=>{
+  console.log("i hit this");
+  console.log(Number(numTotal));
+  console.log(numTotal.split(/([-+*\/])/g));
+
+})
+
+
+
+
+
+
+
