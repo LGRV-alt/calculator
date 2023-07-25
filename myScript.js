57676f80de72bcdeb4cdd228ae69e1dda4b6c1b3
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-07-25 22:25:06 +0100

    reworking logic behind functions

--- myScript.js
+++ myScript.js
@@ -41,22 +41,39 @@ function calc(){
  }
 };
 
-
-
-
-console.log(calc())
-
+//Linking vars to the buttons displayed on html
 const total = document.getElementById("total");
 const op = document.getElementById("op");
+const numberButtons = document.querySelectorAll(".number");
+const opButtons = document.querySelectorAll(".operator");
+
+
+// Event listener attached to each of the number buttons
+numberButtons.forEach((number) => {
+  number.addEventListener("click", ()=>{
+    console.log("this is a number")
+    showNumber(number.textContent)
+  })
+});
+
+// Event listener attached to each of the op buttons
+opButtons.forEach((operator) => {
+  operator.addEventListener("click", ()=>{
+    console.log("this is an operator")
+    showNumber(operator.textContent)
+  })
+})
+
+
+
+
 
 // Capturing all the buttons and attaching event listener to each
-document.querySelectorAll('button').forEach(e =>{
-  e.addEventListener("click", ()=> {
-    console.log(e)
-    digit_pressed(e.textContent);
-    showNumber(e.textContent);
-  });
-});
+// document.querySelectorAll('button').forEach(e =>{
+//   e.addEventListener("click", ()=> {
+//     showNumber(e.textContent);
+//   });
+// });
 
 // This is just to show what button is being pressed 
 function digit_pressed(digit) {
@@ -75,11 +92,51 @@ let numTotal = "";
 
 // Adds event for the equals key
 op.addEventListener("click", ()=>{
-  console.log("i hit this");
-  console.log(Number(numTotal));
-  console.log(numTotal.split(/([-+*\/])/g));
-
+  console.log(numTotal);
+  const total = numTotal.split(/([-+*\/])/g)
+  console.log(total)
+  changeToNum(total)
 })
+
+
+
+// function changeToNum(arr){
+//  let =  newArr = []
+//   for (let i = 0; i < arr.length; i++){
+//     if(isNaN(arr[i])){
+//       operator = arr[i]
+//       console.log(operator)
+//       console.log("This is an operator");
+//       console.log(operator);
+//     } else {
+//       console.log(parseInt(arr[i]))
+//         newNum = parseInt(arr[i])
+//         newArr.push(newNum);
+//         console.log("this is not an operator ")
+//     }
+//   }
+//   };
+
+
+  let currentNum = 8
+  let prevNum = 9
+  let opState = "/"
+  
+
+  function calculate(){
+    if(opState == "/"){
+      currentNum = cal.div(prevNum, currentNum);
+    };
+    return currentNum
+  };
+
+
+  console.log(calculate())
+
+
+
+
+
 
 
 
