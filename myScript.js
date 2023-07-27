a53b4c07f71e93e540b0d9ab3e2e6651ec7c35f4
LGRV-alt <114938920+LGRV-alt@users.noreply.github.com>
2023-07-27 21:42:01 +0100

    Added to event listeners

--- myScript.js
+++ myScript.js
@@ -1,11 +1,3 @@
-num1 = 2
-num2 = 5
-operator ="+"
-
-// console.log(cal.add(num1, num2))
-// console.log(cal.sub(90,30))
-// console.log(cal.div(5,2))
-
 
 
 const cal = {
@@ -43,37 +35,55 @@ function calc(){
 
 //Linking vars to the buttons displayed on html
 const total = document.getElementById("total");
-const op = document.getElementById("op");
+const calEqual = document.getElementById("op")
 const numberButtons = document.querySelectorAll(".number");
 const opButtons = document.querySelectorAll(".operator");
+const ac = document.getElementById("ac");
+
+let runningTotal = ""
+let numTotal = "";
+let currentNum = ""
+let prevNum = ""
+let opState = "/"
+let sumTotal = ""
 
 
 // Event listener attached to each of the number buttons
 numberButtons.forEach((number) => {
   number.addEventListener("click", ()=>{
-    console.log("this is a number")
     showNumber(number.textContent)
+    runningTotal += number.innerHTML
+    currentNum = runningTotal;
+    console.log(runningTotal)
+    result(prevNum, currentNum)
   })
 });
 
 // Event listener attached to each of the op buttons
 opButtons.forEach((operator) => {
   operator.addEventListener("click", ()=>{
-    console.log("this is an operator")
     showNumber(operator.textContent)
+    opState = operator.innerHTML
+    prevNum = currentNum
+    runningTotal = ""
   })
+})
+
+// Event listener for the ac button - 
+ac.addEventListener("click", ()=>{
+  sumTotal = "";
+  currentNum = ""
+  prevNum = ""
+  runningTotal = ""
+  numTotal = 
+  total.innerHTML = sumTotal
 })
 
 
 
 
 
-// Capturing all the buttons and attaching event listener to each
-// document.querySelectorAll('button').forEach(e =>{
-//   e.addEventListener("click", ()=> {
-//     showNumber(e.textContent);
-//   });
-// });
+
 
 // This is just to show what button is being pressed 
 function digit_pressed(digit) {
@@ -84,54 +94,33 @@ function digit_pressed(digit) {
 function showNumber(digit) {
   numTotal += digit
   total.innerHTML = numTotal
-  console.log(numTotal)
 };
 
 
-let numTotal = "";
+
 
 // Adds event for the equals key
 op.addEventListener("click", ()=>{
-  console.log(numTotal);
-  const total = numTotal.split(/([-+*\/])/g)
-  console.log(total)
-  changeToNum(total)
+  console.log(sumTotal)
+  total.innerHTML = sumTotal
 })
 
 
 
-// function changeToNum(arr){
-//  let =  newArr = []
-//   for (let i = 0; i < arr.length; i++){
-//     if(isNaN(arr[i])){
-//       operator = arr[i]
-//       console.log(operator)
-//       console.log("This is an operator");
-//       console.log(operator);
-//     } else {
-//       console.log(parseInt(arr[i]))
-//         newNum = parseInt(arr[i])
-//         newArr.push(newNum);
-//         console.log("this is not an operator ")
-//     }
-//   }
-//   };
-
-
-  let currentNum = 8
-  let prevNum = 9
-  let opState = "/"
   
+// basic function to complete a sum
+function result(num1, num2){
+  // find 
+  if(num1 != "" && num2 != ""){
+    newNum1 = parseFloat(num1);
+    newNum2 = parseFloat(num2);
+    sumTotal = cal.add(newNum1, newNum2);
+    return sumTotal;
+    
+  }
 
-  function calculate(){
-    if(opState == "/"){
-      currentNum = cal.div(prevNum, currentNum);
-    };
-    return currentNum
-  };
+}
 
-
-  console.log(calculate())
 
 
 
